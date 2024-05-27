c8510097be25ad085ee7495efc0e8b1230c88db9
sophiasissi <sophiacurcioguedes@gmail.com>
2024-05-27 09:40:20 -0300

    Arrumando arquivos

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,16 @@ import { useRouter } from "expo-router";
 import { View, Text, TouchableOpacity } from "react-native";
 
 export default function Index() {
-    const router = useRouter()
-    return (
-        <View>
-        <Text>Hello, world!</Text>
-        <TouchableOpacity onPress={() => router.navigate("login")}>
-            <Text>Go to login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => router.navigate("register")}>
-            <Text>Go to registerx</Text>
-        </TouchableOpacity>
-        </View>
-    );
-    }
+  const router = useRouter();
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+      <TouchableOpacity onPress={() => router.navigate("login")}>
+        <Text>Go to login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.navigate("register")}>
+        <Text>Go to registerx</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
