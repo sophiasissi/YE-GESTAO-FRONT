2e6c7a5b703617697428993279b5931ef0b7516a
sophiasissi <sophiacurcioguedes@gmail.com>
2024-05-29 11:35:45 -0300

    creating button

--- app/home/styles.ts
+++ app/home/styles.ts
@@ -13,3 +13,17 @@ export const Logo = styled.Image`
   height: 25%;
   bottom: 30%;
 `
+export const Button = styled.TouchableOpacity`
+    border-radius: 8px;
+    background-color: #000;
+    padding: 10px;
+    width: 80%;
+    align-self: center;
+    margin-top: 25px;
+`
+export const ButtonText = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+`
