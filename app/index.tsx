1095c62a1f0b52d1b5f11be25d7134043e36759e
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 17:45:13 -0300

    Improving login

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,37 @@ import { useRouter } from "expo-router";
 import { Text, TouchableOpacity } from "react-native";
 import { Button, ButtonText, Container, Input, Logo, SecondContainer, SecondText, Txt } from "../app/styles";
 import Logon from "../assets/images/logo_principal.png";
+import { AuthRepositoryHttp } from "@/api/repository/authRepositoryHttp";
+import { useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
 
 export default function Index() {
   const router = useRouter();
+  const authRepository = new AuthRepositoryHttp();
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  async function verify() {
+    await authRepository.verify().then((res) => {
+      if(res.status != 200) {
+        AsyncStorage.removeItem('token')
+        router.navigate("")
+      } else {
+        router.navigate("(tabs)/home")
+      }
+    })
+  }
+
   return (
     <Container>
-      <Logo source={Logon} />
+      <Toast />
+      <Logo source={Logon} resizeMode="contain" />
       <Txt>Bem vindo ao seu app de Gestão de Saúde!</Txt>
-      <Input placeholder={`Insira seu email`} />
-      <Input placeholder={`Insira sua senha`} />
-      <Button onPress={() => router.navigate("(tabs)/home")}>
+      <Input onChangeText={setEmail} placeholder={`Insira seu email`} />
+      <Input onChangeText={setPassword} placeholder={`Insira sua senha`} secureTextEntry />
+      <Button onPress={() => login()}>
         <ButtonText>Entrar</ButtonText>
       </Button>
       <SecondContainer>
