43fe43fcc903383dc9de0f059b0136de3a49a97c
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 17:34:38 -0300

    Upload register

--- app/register/index.tsx
+++ app/register/index.tsx
@@ -3,46 +3,65 @@ import { router } from 'expo-router';
 import { Button, ButtonText, Container, Logo, SecondContainer, SecondText, StyledTextInput } from '../register/styles';
 import { Text, TouchableOpacity } from "react-native";
 import Logon from '../../assets/images/logo_principal.png'
+import { AuthRepositoryHttp, User } from '@/api/repository/authRepositoryHttp';
+import Toast from 'react-native-toast-message';
 
 export default function Register() {
+  const authRepository = new AuthRepositoryHttp();
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    phone: ''
-  });
-
-  const handleChange = (key: string, value: string) => {
-    setFormData({ ...formData, [key]: value });
-  };
+  const [name, setName] = useState<string>('')  
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+  const [phone, setPhone] = useState<string>('')
 
   const handleSignup = () => {
-    console.log('Sign up:', formData);
-  };
+    if(password !== confirmPassword) {
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao cadastrar!',
+        text2: 'As senhas não são iguais!'
+      });
+      return;
+    }
 
-  const fields = [
-    { placeholder: 'Nome', key: 'name', secureTextEntry: false, keyboardType: 'default' },
-    { placeholder: 'Email', key: 'email', secureTextEntry: false, keyboardType: 'email-address' },
-    { placeholder: 'Senha', key: 'password', secureTextEntry: true, keyboardType: 'default' },
-    { placeholder: 'Confirmar Senha', key: 'confirmPassword', secureTextEntry: true, keyboardType: 'default' },
-    { placeholder: 'Telefone', key: 'phone', secureTextEntry: false, keyboardType: 'phone-pad' },
-  ];
+    const json: User = {
+      name: name,
+      email: email,
+      password: password,
+      phone: phone
+    }
+    console.log(json);
+    authRepository.cadastro(json).then((res) => {
+      if(res.status != 201) {
+        Toast.show({
+          type: 'error',
+          text1: 'Erro ao cadastrar!',
+          text2: res.data.message
+        });
+        alert(res.data.message)
+      }else {
+        Toast.show({
+          type: 'success',
+          text1: 'Cadastrado com sucesso!'
+        })
+        setTimeout(() => {
+          router.navigate("")
+        }, 3000);
+      }
+    })
+  };
 
   return (
     <Container>
+      <Toast />
       <Logo source={Logon} />
-      {fields.map((field) => (
-        <StyledTextInput
-          key={field.key}
-          placeholder={field.placeholder}
-          value={formData[field.key as keyof typeof formData]}
-          onChangeText={(text) => handleChange(field.key, text)}
-          secureTextEntry={field.secureTextEntry}
-        />
-      ))}
-      <Button onPress={() => router.navigate("")}>
+      <StyledTextInput placeholder='Nome' onChangeText={setName} />
+      <StyledTextInput placeholder='Email' onChangeText={setEmail} />
+      <StyledTextInput placeholder='Telefone' onChangeText={setPhone} keyboardType='phone-pad' maxLength={11}/>
+      <StyledTextInput placeholder='Senha' onChangeText={setPassword} secureTextEntry />
+      <StyledTextInput placeholder='Confirmar Senha' onChangeText={setConfirmPassword} secureTextEntry />
+      <Button onPress={() => handleSignup()}>
         <ButtonText>Cadastrar</ButtonText>
       </Button>
       <SecondContainer>
