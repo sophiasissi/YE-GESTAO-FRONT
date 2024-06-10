c864f7c105e5eff91d400f8a29a48c8e4cf59fe8
Alexandre Giuliani Bresan <167318675+alexandrebresan@users.noreply.github.com>
2024-06-10 20:19:36 -0300

    AddindFunctionMedication

--- app/addMedication/index.tsx
+++ app/addMedication/index.tsx
@@ -7,3 +7,37 @@ import { FontAwesome6 } from "@expo/vector-icons";
 import MaskInput from "react-native-mask-input";
 import { MedicationRepositoryHttp } from "@/api/repository/medicationRepositoryHttp";
 import Toast from "react-native-toast-message";
+
+export default function AddMedication() {
+    const router = useRouter();
+    const medicationRepository = new MedicationRepositoryHttp();
+
+    const [modal, setModal] = useState(false);
+    const [name, setName] = useState('');
+    const [hour, setHour] = useState('');
+    const [period, setPeriod] = useState('');
+    const [interval, setInterval] = useState('');
+
+    async function saveMedication(){
+        if(name === '' || hour === '' || period === '' || interval === ''){
+            return;
+        }
+
+        const data = {
+            'name': name,
+            'hour': hour,
+            'period': period,
+            'interval': interval
+        }
+
+        medicationRepository.createMedication(data).then((response) => {
+            // console.log(response);
+            if(response.status === 201){
+                openModal();
+            } else {
+                alert('Erro ao salvar')
+            }
+        }).catch((error: any) => {
+            console.log(error);
+        });
+    }
