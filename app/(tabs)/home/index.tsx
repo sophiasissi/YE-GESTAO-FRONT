741372d3e4e2b7fab1da681b7d7ec6f3e14d7860
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 21:23:14 -0300

    Uploading Home

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -25,3 +25,42 @@ export default function Home() {
     { title: 'Glicemia', route: 'glucose' },
     { title: 'IMC', route: 'imc' },
   ];
+
+  useEffect(() => {
+    verify()
+  }, [])
+
+  return (    
+    <View>
+      <Logo source={Logon} />
+      <CardContainer>
+        {cards.map((card, index) => (
+          <Link
+            href={`multiscreen/${card.route}`}
+            key={index}
+            style={styles.card}
+          >
+            <TxtCard>{card.title}</TxtCard>
+          </Link>
+  ))}
+      </CardContainer>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#739489',
+    padding: 20,
+    borderRadius: 8,
+    marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    width: 350,
+    height: 60,
+  },
+});
