bc63d5234c9b0f05901bd287c07f5206d98b7fa5
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 21:52:15 -0300

    Adding Multi Repository

--- api/repository/multiRepositoryHttp.ts
+++ api/repository/multiRepositoryHttp.ts
@@ -48,3 +48,51 @@ export class MultiRepositoryHttp {
             return error.response;
         }
     }
+
+    async createGlucose(data: any) {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await api.post('/glucose', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response;
+        }
+        catch (error:any) {
+            console.log(error);
+            return error.response;
+        }
+    }
+
+    async getImc() {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await api.get('/imc', {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+        });
+            return response;
+        } catch (error:any) {
+            console.log(error);
+            return error.response;
+        }
+    }
+
+    async createImc(data: any) {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await api.post('/imc', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response;
+        }
+        catch (error:any) {
+            console.log(error);
+            return error.response;
+        }
+    }
+}
