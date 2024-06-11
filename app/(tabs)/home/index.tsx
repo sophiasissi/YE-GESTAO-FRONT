d306416615b7faf33ab6b383d829b9ebdee06241
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 21:18:18 -0300

    Adding Home

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,51 +1,27 @@
-import { useRouter } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { View, CardContainer, Logo, TxtCard } from "./styles";
 import Logon from "../../../assets/images/logo_escuro.png";
-import * as React from 'react';
+import { useEffect } from 'react';
 import { TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { AuthRepositoryHttp } from "@/api/repository/authRepositoryHttp";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export default function Home() {
   const router = useRouter();
+  const authRepository = new AuthRepositoryHttp();
+
+  async function verify() {
+    await authRepository.verify().then((res) => {
+      if(res.status != 200) {
+        AsyncStorage.removeItem('token')
+        router.navigate("")
+      }
+    })
+  }
 
   const cards = [
     { title: 'Pressão', route: 'pressure' },
     { title: 'Glicemia', route: 'glucose' },
-    { title: 'Peso e Altura', route: 'weight-height' },
-    { title: 'IMC', route: 'bmi' },
+    { title: 'IMC', route: 'imc' },
   ];
-
-  return (    
-    <View>
-      <Logo source={Logon} />
-      <CardContainer>
-        {cards.map((card, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.card}
-            onPress={() => router.push(`/form/${card.route}`)}
-          >
-            <TxtCard>{card.title}</TxtCard>
-          </TouchableOpacity>
-  ))}
-      </CardContainer>
-    </View>
-
-  );
-}
-
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#739489',
-    padding: 20,
-    borderRadius: 8,
-    marginVertical: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
-    width: 350,
-    height: 60,
-  },
-});
