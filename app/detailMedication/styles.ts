2354bec77fb4633b8274550dbf604acf7225e847
Alexandre Giuliani Bresan <167318675+alexandrebresan@users.noreply.github.com>
2024-06-11 12:44:04 -0300

    StylingMedicationDetails

---
+++ app/detailMedication/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    padding: 8px;
+    background-color: #f5f5f5;
+`;
+
+export const ViewText = styled.View`
+    background-color: #fff;
+    padding: 16px;
+    margin-bottom: 4px;
+`;
