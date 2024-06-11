d98fb40858d7297d5a7f436669dc63fdbd490bb4
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-10 21:12:10 -0300

    Uploading Consultations

--- app/(tabs)/consultations/index.tsx
+++ app/(tabs)/consultations/index.tsx
@@ -1,25 +1,11 @@
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { Container } from './styles';
-import { FontAwesome6 } from "@expo/vector-icons";
-import { ConsultationCard } from '@/components/ConsultationCard';
-import { useFocusEffect, useRouter } from 'expo-router';
-import { ConsultationRepositoryHttp } from '@/api/repository/consultationRepositoryHttp';
-import { useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AuthRepositoryHttp } from '@/api/repository/authRepositoryHttp';
+import { Text } from 'react-native'
 
 export default function Consultations() {
-    const router = useRouter();
-    const consultationRepository = new ConsultationRepositoryHttp();
-    const authRepository = new AuthRepositoryHttp();
+    return (
+        <Container>
 
-    const [data, setData] = useState([]);
+        </Container>
 
-    async function verify() {
-        await authRepository.verify().then((res) => {
-          if(res.status != 200) {
-            AsyncStorage.removeItem('token')
-            router.navigate("")
-          }
-        })
-      }
+    );
+}
