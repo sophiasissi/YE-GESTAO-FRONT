4eac44a8e83e6dfc5b5782559ccf961d22af1dc0
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-11 16:24:02 -0300

    Updating Medication

--- app/(tabs)/medication/index.tsx
+++ app/(tabs)/medication/index.tsx
@@ -1,11 +1,48 @@
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import { Container } from './styles';
-import { Text } from 'react-native'
+import { FontAwesome6 } from "@expo/vector-icons";
+import { MedicationCard } from '@/components/MedicationCard';
+import { useFocusEffect, useRouter } from 'expo-router';
+import { MedicationRepositoryHttp } from '@/api/repository/medicationRepositoryHttp';
+import { useEffect, useState } from 'react';
 
 export default function Medication() {
+    const router = useRouter();
+    const medicationRepository = new MedicationRepositoryHttp();
+
+    const [jsonList, setJsonList] = useState([]);
+
+    async function getMedications() {
+        await medicationRepository.getMedications().then((response) => {
+            // console.log(response);
+            setJsonList(response.data);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    useFocusEffect(() => {
+        getMedications();
+    })
+    useEffect(() => {
+        getMedications();
+    },[])
+
     return (
         <Container>
-
+            <FlatList
+                data={jsonList}
+                renderItem={({item}) => {
+                    return (
+                        <MedicationCard json={item}/>
+                    );
+                }}
+            />
+            <View style={{alignItems:'center', marginVertical: 8}}>
+                <TouchableOpacity onPress={()=>router.navigate('addMedication')}>
+                    <FontAwesome6 name="circle-plus" size={40} color="black" />
+                </TouchableOpacity>
+            </View>
         </Container>
-
     );
 }
